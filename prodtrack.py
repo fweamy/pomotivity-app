b56e576ba5df57420e857e25a669c7abcffac7a0
fweamy <baohanmi@gmail.com>
2025-01-03 23:21:49 -0500

    Connecting mouse event test

--- prodtrack.py
+++ prodtrack.py
@@ -1,22 +1,30 @@
+import sys
+
 from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QVBoxLayout, QWidget, QPushButton
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QTextEdit, QLineEdit, QVBoxLayout, QWidget, QPushButton
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("ProdTrack")
 
-        self.label = QLabel()
-
-        self.input = QLineEdit()
-        self.input.textChanged.connect(self.label.setText)
-
-        self.setCentralWidget(self.input)
+        self.label = QLabel("Click in this window")
         self.setCentralWidget(self.label)
 
+    def mouseMoveEvent (self, e):
+        self.label.setText("mouseMoveEvent")
+    
+    def mousePressEvent(self, e):
+        self.label.setText("mousePressEvent")
+
+    def mouseReleaseEvent(self, e):
+        self.label.setText("mouseReleaseEvent")
+
+    def mouseDoubleClickEvent(self, e):
+        self.label.setText("mouseDoubleClickEvent")
         
 
-app = QApplication([])
+app = QApplication(sys.argv)
 
 window = MainWindow()
 window.show()
