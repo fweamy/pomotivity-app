7331942e7b7946ab3187f76c8195f47da040f58e
fweamy <baohanmi@gmail.com>
2025-01-07 12:08:04 -0500

    Nested layout test

--- prodtrack.py
+++ prodtrack.py
@@ -9,13 +9,29 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.setWindowTitle("ProdTrack")
 
-        layout = QHBoxLayout()
-        layout.addWidget(Color("red"))        
-        layout.addWidget(Color("blue"))
-        layout.addWidget(Color("yellow"))
+
+        layout1 = QHBoxLayout()
+        layout2 = QVBoxLayout()
+        layout3 = QVBoxLayout()
+
+        layout2.addWidget(Color("red"))
+        layout2.addWidget(Color("yellow"))
+        layout2.addWidget(Color("purple"))
+
+        layout1.setContentsMargins(0,0,0,0)
+        layout1.setSpacing(0)
+
+        layout1.addLayout(layout2)
+
+        layout1.addWidget(Color("green"))
+
+        layout3.addWidget(Color("red"))
+        layout3.addWidget(Color("purple"))
+
+        layout1.addLayout(layout3)
 
         widget = QWidget()
-        widget.setLayout(layout)
+        widget.setLayout(layout1)
     
         self.setCentralWidget(widget)
 
